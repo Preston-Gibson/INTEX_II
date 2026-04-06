e5f0640ee19a95cd88e3ab2e8da5d8295d770d18
Preston <prestong378@gmail.com>
2026-04-06 17:52:47 -0600

    Fixing merge conflicts and making both pages compatable with changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,12 @@
-import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 import ImpactSection from '../components/ImpactSection'
 import MapSection from '../components/MapSection'
 import HowWeWork from '../components/HowWeWork'
 import CTASection from '../components/CTASection'
-import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <>
-      <Nav />
       <main>
         <Hero />
         <ImpactSection />
@@ -17,7 +14,6 @@ export default function Home() {
         <HowWeWork />
         <CTASection />
       </main>
-      <Footer />
     </>
   )
 }
