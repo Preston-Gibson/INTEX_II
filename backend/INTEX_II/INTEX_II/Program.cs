9e8c31f68989637ddafd65d9e547f30cc2733f28
jakejwright27 <mrjdawg@byu.edu>
2026-04-07 17:48:08 -0600

    params

--- backend/INTEX_II/INTEX_II/Program.cs
+++ backend/INTEX_II/INTEX_II/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
